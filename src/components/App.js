e5d0e0176218d28775e64e8a1179c0e11cc6f352
Bhavika Rathod <bhavikarathod2117@gmail.com>
2023-06-11 19:08:31 +0530

    my first react assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
+
+const[count, setCount] = useState(0)
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>{`Button clicked ${count} times`}</p>
+        <button onClick ={()=>setCount(count+1)} id = "btn"></button>
     </div>
   )
 }
